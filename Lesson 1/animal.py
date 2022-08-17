3399b475e4d1e67582c818a6f98462f368a44117
MaxJuriss <103474822+MaxJuriss@users.noreply.github.com>
2022-08-18 09:59:20 +1200

    Add files via upload

---
+++ Lesson 1/animal.py
@@ -0,0 +1,13 @@
+# variables
+name='lion'
+numLegs=4
+hasFur='True' 
+height=1.2
+
+# Prepare print statements
+print(type(name))
+print(type(numLegs))
+print(type(hasFur))
+print(type(height)) 
+ 
+# Construct a readable sentence   
