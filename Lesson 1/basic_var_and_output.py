9804daf98e7308ba194d4526d80907aab3e9c600
MaxJuriss <103474822+MaxJuriss@users.noreply.github.com>
2022-08-25 09:49:33 +1200

    Add files via upload

---
+++ Lesson 1/basic_var_and_output.py
@@ -0,0 +1,22 @@
+#M.Juriss
+# variables
+
+#Declaration of variables
+name='lion'
+numLegs=4
+hasFur='True' 
+height=1.2
+
+# Prepare print statements
+print(type(name))
+print(type(numLegs))
+print(type(hasFur))
+print(type(height)) 
+
+# Output to screen
+print(name)
+print(numLegs)
+print(hasFur)
+print(height)
+
+# Construct a readable sentence   
---
+++ Lesson 1/basic_var_output_show_Labels.py
@@ -0,0 +1,28 @@
+#M.Juriss
+# variables, output, Labels
+
+#Declaration of variables
+name='lion'
+numLegs=4
+hasFur='True' 
+height=1.2
+
+# Prepare print statements
+print(type(name))
+print(type(numLegs))
+print(type(hasFur))
+print(type(height)) 
+
+# Output to screen
+print("One of the big five animals in africa is called an",name,'.')
+print("An",name,"has",numLegs,"legs.")
+print("Its height is approximately",height,"metres.")
+print("And, yes, it's",hasFur,"It has fur")
+print("And, yes ,it's",hasFur,"It has fur.")
+# replacing a variable value
+print(name)
+print(numLegs)
+print(hasFur)
+print(height)
+
+# Construct a readable sentence   
